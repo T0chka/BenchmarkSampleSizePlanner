f758e9890cb423ceeea349b8865988b9ca364867
T0chka <an.dolgorukova@gmail.com>
2025-08-19 00:17:54 +0300

    code organized

--- css.R
+++ css.R
@@ -1,4 +1,4 @@
-
+# Theme Configuration ----------------------------------------------------------
 
 theme <- bs_theme(
   version = 5,
@@ -9,11 +9,14 @@ theme <- bs_theme(
   heading_font = font_google("Inter")
 )
 
-# H5
+# Navigation -------------------------------------------------------------------
+
+# Navbar brand styling (Title and subtitle)
 theme <- bslib::bs_add_rules(theme, "
-  h5, .h5 {
-    font-weight: 500;
-    font-size: 1rem !important;
+  .navbar .navbar-brand { margin: 0 !important; }
+  .navbar .navbar-brand .name-title   { font-size: 2rem; }
+  .navbar .navbar-brand .name-subtitle{
+    font-size: 1rem; font-weight: 400; color: var(--bs-gray-500);
   }
 ")
 
@@ -23,33 +26,7 @@ theme <- bslib::bs_add_rules(theme, "
     --bs-nav-link-color: var(--bs-primary-text-emphasis) !important;
     --bs-nav-link-hover-color: var(--bs-primary);
   }
-  "
-)
-
-# Left panel input blocks spacing
-theme <- bslib::bs_add_rules(theme, "
-  .input-block { margin-bottom: .75rem; display: flow-root; }
-  .input-block > h5 { margin: 0 0 .25rem 0; font-size: 1.25rem; }
-  
-  /* Ensure no special-casing for last block margins */
-  .input-block:last-child { margin-bottom: .75rem !important; }
-  "
-)
-
-# The gap between columns
-theme <- bslib::bs_add_rules(theme, "
-  .cols-tight {
-    --tight-gap: 0.75rem;
-    gap: var(--tight-gap) !important;
-  }
-  "
-)
-
-# Sidebar height
-theme <- bslib::bs_add_rules(theme, "
-  .sidebar-card { min-height: 100vh; }
-  "
-)
+")
 
 # Match card-header style to accordion-button
 theme <- bslib::bs_add_rules(theme, "
@@ -60,51 +37,44 @@ theme <- bslib::bs_add_rules(theme, "
   "
 )
 
-# Title and subtitle
-theme <- bslib::bs_add_rules(theme, "
-  .navbar .navbar-brand {
-    margin: 0 !important;
-  }
+# Layout & Spacing -------------------------------------------------------------
 
-  .navbar .navbar-brand .name-title {
-  font-size: 2rem;
+# Height of the page
+theme <- bslib::bs_add_rules(theme, "
+  .html-fill-container {
+    min-height: auto !important;
   }
-  .navbar .navbar-brand .name-subtitle {
-  font-size: 1rem; font-weight: 400; color: var(--bs-gray-500);
+")
+
+# Bottom spacing for content
+theme <- bslib::bs_add_rules(theme, "
+  /* Apply bottom spacing only to the main content container (after navbar) */
+  .bslib-page-navbar > .navbar + .container-fluid {
+    padding-bottom: 2.5rem;
+  }
+")
+
+# Left panel input blocks spacing
+theme <- bslib::bs_add_rules(theme, "
+  .input-block { margin-bottom: .75rem; display: flow-root; }
+  .input-block > h5 { margin: 0 0 .25rem 0; font-size: 1.25rem; }
+  
+  /* Ensure no special-casing for last block margins */
+  .input-block:last-child { margin-bottom: .75rem !important; }
+  
+  /* Remove row gap (between settings) */
+  .bslib-card .card-body{
+    row-gap: 0;
   }
   "
 )
 
-# Sticky footer
+# Column layout
 theme <- bslib::bs_add_rules(theme, "
-  body { padding-bottom: 32px; }
-  .app-footer {
-    position: fixed; left: 0; right: 0; bottom: 0;
-    background: var(--bs-body-bg);
-    padding-top: 0.5rem !important;
-    padding-bottom: 0.5rem !important;
-    border-top: 1px solid var(--bs-border-color);
-    text-align: center;
-    font-size: 0.85rem;
-    color: #6c757d;
-  }
-  "
-)
-
-# Remove default extra borders on active accordion-button
-theme <- bslib::bs_add_rules(theme, "
-  .accordion-button {
-    border: none !important;
-    box-shadow: none !important;
-  }
-  .accordion-button:not(.collapsed) {
-    border: none !important;
-    box-shadow: none !important;
-  }
-  .accordion-button:focus {
-    border: none !important;
-    box-shadow: none !important;
-    outline: none !important;
+  /* Column gap */
+  .layout-columns {
+    --tight-gap: 1rem;
+    gap: var(--tight-gap) !important;
   }
 ")
 
@@ -117,20 +87,50 @@ theme <- bslib::bs_add_rules(theme, "
   "
 )
 
-# Gap spacing
+# Footer -----------------------------------------------------------------------
+
 theme <- bslib::bs_add_rules(theme, "
-  .bslib-gap-spacing {
-    gap: 0;
+  body { padding-bottom: 48px; }
+  .app-footer {
+    position: fixed; 
+    left: 0; 
+    right: 0; 
+    bottom: 0;
+    background: var(--bs-body-bg);
+    padding-top: 0.5rem !important;
+    padding-bottom: 0.5rem !important;
+    border-top: 1px solid var(--bs-border-color);
+    text-align: center;
+    font-size: 0.70rem;
+    color: #6c757d;
+    z-index: 1030;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
-  "
-)
+")
+
+# Accordion --------------------------------------------------------------------
+
+# Accordion: remove extra borders/shadows
+theme <- bslib::bs_add_rules(theme, "
+  .accordion-button,
+  .accordion-button:not(.collapsed),
+  .accordion-button:focus {
+    border: none !important;
+    box-shadow: none !important;
+    outline: none !important;
+  }
+")
+
+# Results rection --------------------------------------------------------------
 
 # value-box
 theme <- bslib::bs_add_rules(theme, "
   .value-box {
     border: 0 !important;
     border-radius: 14px;
-    background: rgba(11,114,133,.06);           /* #0B7285 с малой насыщенностью */
+    background: rgba(11,114,133,.06);           /* Primary color with low opacity */
     box-shadow: inset 0 0 0 1px rgba(11,114,133,.12);
   }
   .value-box .value-box-title {
@@ -155,6 +155,8 @@ theme <- bslib::bs_add_rules(theme, "
   }
   "
 )
+
+# Tooltips ---------------------------------------------------------------------
 
 # Help hint and info icon styles for tooltips
 theme <- bslib::bs_add_rules(theme, "
